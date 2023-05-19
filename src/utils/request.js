d7a992be8123b3ac86d255337ab711cbe6619df4
yt-tian <1074965187@qq.com>
2023-05-19 14:41:11 +0800

    提交代码

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,26 +1,20 @@
-/****   request.js   ****/
-// 导入axios
+
 import axios from 'axios'
 
 import { ElMessage  } from 'element-plus';
-//1. 创建新的axios实例，
 const service = axios.create({
   // 公共接口
   baseURL: '/api/v1',
-  // 超时时间 单位是ms，这里设置了3s的超时时间
   timeout: 3 * 1000
 })
-// 2.请求拦截器
 service.interceptors.request.use(config => {
-  //发请求前做的一些处理，数据转化，配置请求头，设置token,设置loading等，根据需求去添加
-   config.data = JSON.stringify(config.data); //数据转化,也可以使用qs转换
+   config.data = JSON.stringify(config.data); 
    config.headers = {
-     'Content-Type':'application/x-www-form-urlencoded' //配置请求头
+     'Content-Type':'application/x-www-form-urlencoded' 
    }
-   //注意使用token的时候需要引入cookie方法或者用本地localStorage等方法，推荐js-cookie
-   const token = sessionStorage.getItem('Authorization');//这里取token之前，你肯定需要先拿到token,存一下
+   const token = sessionStorage.getItem('Authorization');
    if(token){
-      config.headers.token= token; //如果要求携带在请求头中
+      config.headers.token= token; 
     }
   return config
 }, error => {
@@ -29,14 +23,9 @@ service.interceptors.request.use(config => {
 
 // 3.响应拦截器
 service.interceptors.response.use(response => {
-  //接收到响应数据并成功后的一些共有的处理，关闭loading等
-  
   return response
 }, error => {
-   /***** 接收到异常响应的处理开始 *****/
   if (error && error.response) {
-    // 1.公共错误处理
-    // 2.根据响应码具体处理
     switch (error.response.status) {
       case 400:
         error.message = '错误请求'
@@ -49,7 +38,6 @@ service.interceptors.response.use(response => {
         break;
       case 404:
         error.message = '请求错误,未找到该资源'
-        // window.location.href = "/NotFound"
         break;
       case 405:
         error.message = '请求方法未允许'
@@ -79,16 +67,13 @@ service.interceptors.response.use(response => {
         error.message = `连接错误${error.response.status}`
     }
   } else {
-    // 超时处理
     if (JSON.stringify(error).includes('timeout')) {
       ElMessage .error('服务器响应超时，请刷新当前页')
     }
     error.message = '连接服务器失败'
   }
 
-  ElMessage .error(error.message)
-  /***** 处理结束 *****/
-  //如果不需要错误处理，以上的处理过程都可省略
+  ElMessage .error(error.message) 
   return Promise.resolve(error.response)
 })
 //4.导出axios实例
